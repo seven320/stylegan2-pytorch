c7339bb0391ba5a134b4f32701116b4ca565076c
ken <yosyuaomenw@yahoo.co.jp>
2022-03-21 18:14:59 +0900

    change ckpt to every 5000

--- train.py
+++ train.py
@@ -314,7 +314,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                         range=(-1, 1),
                     )
 
-            if i % 10000 == 0:
+            if i % 5000 == 0:
                 torch.save(
                     {
                         "g": g_module.state_dict(),
